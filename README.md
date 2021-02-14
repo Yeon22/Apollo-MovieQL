a949a24601db89f7dd431b434f4cd3a08dcba9d8
Suyeon Bak <bsuyeon16@gmail.com>
2021-02-14 16:28:06 +0900

    create apollo client

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Apollo MovieQL
 
 Movie App build with React, Apollo and GraphQL
+Deploy Heroku.
+
+Go to [Demo](https://apollo-movieql.herokuapp.com/)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import {ApolloProvider} from '@apollo/client';
+import client from './apollo';
 
 ReactDOM.render(
+  <ApolloProvider client={client}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
